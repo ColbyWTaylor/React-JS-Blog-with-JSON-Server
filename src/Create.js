f1da410c948919ad8bd478ee0e14e89a9eca4a75
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-28 12:35:11 -0500

    blog create function added, modifies db.json

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("asdfasd");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+    });
+  };
+
   return (
     <div className="create">
       <h2>Create a New Blog</h2>
+      <form onSubmit={handleSumbit}>
+        <label>Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        {(!isPending && <button>Submit</button>) || <button>Working...</button>}
+
+        <h1>Preview:</h1>
+        <h2>{title}</h2>
+        <p>by {author}</p>
+        <p>{body}</p>
+      </form>
     </div>
   );
 };
