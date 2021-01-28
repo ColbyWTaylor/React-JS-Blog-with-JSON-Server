9291f869fb567ec873fcd46aa0a097d5baaa5112
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-28 09:27:25 -0500

    blog details fetch

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,11 +3,21 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data, error, isPending } = useFetch();
+  const { data: blog, error, isPending } = useFetch(
+    "http://localhost:8000/blogs/" + id
+  );
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id} </h2>
+      {isPending && <div>is Loading...</div>}
+      {error && <div> {error} </div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
