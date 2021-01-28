24912d304ddaf53fba97cc66f055dcb72c9dc9d1
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-27 23:54:30 -0500

    bloglist separation

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,8 @@ const BlogList = ({ blogs, title }) => {
       {blogs.map((blog) => (
         <div key={blog.id} className="blog-preview">
           <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <hr />
+          <p>Written by {blog.author}.</p>
           <div className="blog-body">{blog.body}</div>
           <Link to={`/blogs/${blog.id}`}>Go to post</Link>
         </div>
